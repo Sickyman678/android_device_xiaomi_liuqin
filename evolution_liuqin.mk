5d71efb8a4c96ab76ef359d7c2609120139b45f4
Sickyman678 <97682657+Sickyman678@users.noreply.github.com>
2023-08-27 22:40:31 +0100

    Update evolution_liuqin.mk
    Bringup EVO X

--- evolution_liuqin.mk
+++ evolution_liuqin.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+#Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 
 # Inherit product specific makefiles
@@ -20,13 +23,11 @@ $(call inherit-product, vendor/evolution/config/common_full_tablet_wifionly.mk)
 
 # All components inherited here go to vendor image
 #
-# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 
 
-# Device Identifyer 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
