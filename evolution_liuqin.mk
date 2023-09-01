eeced29017d1724a7f64ed3c794e61a2945c8dde
Sickyman678 <97682657+Sickyman678@users.noreply.github.com>
2023-09-01 12:40:40 +0100

    INTIALISE EVO X

--- evolution_liuqin.mk
+++ evolution_liuqin.mk
@@ -1,33 +1,26 @@
-#               2022 The Evolution X Project
+#
+# Copyright (C) 2023 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
-
-
-#Inherit common products
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-
-# Inherit product specific makefiles
-$(call inherit-product, device/xiaomi/liuqin/device.mk)
-$(call inherit-product, vendor/xiaomi/liuqin/liuqin-vendor.mk)
-
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_NAME := evolution_liuqin
-PRODUCT_DEVICE := liuqin
-PRODUCT_MODEL := Tab 6 Pro
-
-# Inherit some common EvolutionX stuff
-$(call inherit-product, vendor/evolution/config/common_full_tablet_wifionly.mk)
-
-# All components inherited here go to vendor image
 #
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from liuqin device
+$(call inherit-product, device/xiaomi/liuqin/device.mk)
 
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_tablet_wifionly.mk)
 
-# GMS
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := evolution_liuqin
+PRODUCT_DEVICE := liuqin
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Pad 6 Pro
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
