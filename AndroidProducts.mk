a6767c88611a79a67e608fff3983a42e83c5ece0
Sickyman678 <97682657+Sickyman678@users.noreply.github.com>
2023-09-01 12:40:08 +0100

    Intiliase evo x

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/evolution_liuqin.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_liuqin-eng \
+    evolution_liuqin-user \
     evolution_liuqin-userdebug \
-    evolution_liuqin-user
+    evolution_liuqin-eng
